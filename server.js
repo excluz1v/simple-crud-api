69d3a60dfad1d4b78b6728e43a951848ce35338f
excluz1v <bmd4work@gmail.com>
2021-11-25 23:29:17 +0600

    feat: add Router class

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const http = require('http')
+const http = require("http")
+const { Router, emitter } = require("./Router")
 
 const host = "localhost"
 const port = "8000"
+const router = new Router()
 
 function requestListener(req, res) {
-    res.setHeader('Content-Type', 'appliction/json')
+
+    const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res)
+
+    if (!emitted) {
+        res.end()
+    }
+    // res.writeHead(200, { "Content-Type": "application/json" })
 }
+
+router.get('/users', (req, res) => {
+    res.end('YOU SEND REQUEST TO /USERS')
+})
+router.get('/posts', (req, res) => {
+    res.end('YOU SEND REQUEST TO /posts')
+})
 
 const server = http.createServer(requestListener)
 
