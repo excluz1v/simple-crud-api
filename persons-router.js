1f25abcaa89f30a084fcc0b897978e54bebd799e
excluz1v <bmd4work@gmail.com>
2021-11-27 14:30:22 +0600

    feat: add /person errors handling with 500 status

--- persons-router.js
+++ persons-router.js
@@ -12,65 +12,94 @@ let persons = require("./persons")
 //     'hobbies': 'arr'
 // }
 
-router.get('/person', (req, res) => {
-    res.writeHead(200, {
-        'Content-type': 'application/json'
-    })
-    res.end(JSON.stringify(persons))
-})
-
-router.get('/person/', (req, res) => {
-    const personId = req.personId
-    if (!validateUUID(personId, res)) return false
-    if (!isExist(personId, res, persons)) return false
-
-    if (persons.find(person => person.id === personId)) {
-        const person = persons.find(person => person.id === personId)
+router.get('/person', async (req, res) => {
+    try {
         res.writeHead(200, {
             'Content-type': 'application/json'
         })
-        res.end(JSON.stringify(person))
+        res.end(JSON.stringify(persons))
+    } catch (err) {
+        res.writeHead(500)
+        res.end(err)
+    }
+
+})
+
+router.get('/person/', (req, res) => {
+    try {
+        const personId = req.personId
+        if (!validateUUID(personId, res)) return false
+        if (!isExist(personId, res, persons)) return false
+
+        if (persons.find(person => person.id === personId)) {
+            const person = persons.find(person => person.id === personId)
+            res.writeHead(200, {
+                'Content-type': 'application/json'
+            })
+            res.end(JSON.stringify(person))
+        }
+    } catch (err) {
+        res.writeHead(500)
+        res.end(err)
     }
 })
 
 router.post('/person', async (req, res) => {
-    const body = JSON.parse(req.body)
-    if (!validateTypes(body, res)) return false
-    const newPerson = create(body)
-    persons = [...persons, newPerson]
-    res.writeHead(201, {
-        'Content-type': 'application/json'
-    })
-    res.end(JSON.stringify(newPerson))
+
+    try {
+        const body = JSON.parse(req.body)
+        if (!validateTypes(body, res)) return false
+        const newPerson = create(body)
+        persons = [...persons, newPerson]
+        res.writeHead(201, {
+            'Content-type': 'application/json'
+        })
+        res.end(JSON.stringify(newPerson))
+    } catch (err) {
+        res.writeHead(500)
+        res.end(err)
+    }
+
 })
 
 router.delete('/person/', async (req, res) => {
-    const personId = req.personId
-    if (!validateUUID(personId, res)) return false
-    if (!isExist(personId, res, persons)) return false
+    try {
+        const personId = req.personId
+        if (!validateUUID(personId, res)) return false
+        if (!isExist(personId, res, persons)) return false
 
-    persons = del(personId, persons)
+        persons = del(personId, persons)
 
-    res.writeHead(204, {
-        'Content-type': 'application/json'
-    })
-    res.end()
+        res.writeHead(204, {
+            'Content-type': 'application/json'
+        })
+        res.end()
+    } catch (err) {
+        res.writeHead(500)
+        res.end(err)
+    }
 })
 
 router.put('/person/', async (req, res) => {
-    const personId = req.personId
-    const body = JSON.parse(req.body)
+    try {
+        const personId = req.personId
+        const body = JSON.parse(req.body)
 
-    if (!validateTypes(body, res)) return false
-    if (!validateUUID(personId, res)) return false
-    if (!isExist(personId, res, persons)) return false
+        if (!validateTypes(body, res)) return false
+        if (!validateUUID(personId, res)) return false
+        if (!isExist(personId, res, persons)) return false
 
-    const [updatedPersons, newPerson] = update(body, personId, persons)
-    persons = updatedPersons
-    res.writeHead(201, {
-        'Content-type': 'application/json'
-    })
-    res.end(JSON.stringify(newPerson))
+        const [updatedPersons, newPerson] = update(body, personId, persons)
+        persons = updatedPersons
+        res.writeHead(201, {
+            'Content-type': 'application/json'
+        })
+        res.end(JSON.stringify(newPerson))
+    } catch (err) {
+        res.writeHead(500)
+        res.end(err)
+    }
+
 })
 
 module.exports = router
